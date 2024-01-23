1fc61f609db0d9f41e921425179f42cdb7954c1c
Vooyage <148488760+Vygaee@users.noreply.github.com>
2024-01-23 17:03:48 +0700

    Update env.py

--- env.py
+++ env.py
@@ -7,7 +7,7 @@ API_ID = os.getenv("API_ID", "26724473").strip()
 API_HASH = os.getenv("API_HASH", "7bc7d1f9b2f3d3f1bfd272db56ac0ba1").strip()
 BOT_TOKEN = os.getenv("BOT_TOKEN", "6949687629:AAGkSP6c8MQwje4AuppaJac_1FIXkBDIBg8").strip()
 DATABASE_URL = os.getenv("DATABASE_URL", "postgres://igzvmvzd:4G6i3O51PB9ZBSO0RcArUlcBBoWMv-es@bubble.db.elephantsql.com/igzvmvzd").strip()
-MUST_JOIN = os.getenv("MUST_JOIN", "@Cari_Teman_Online_Sosmed")
+MUST_JOIN = os.getenv("MUST_JOIN", "@SpotifyStreamMusic")
 
 if not API_ID:
     print("No API_ID found. Exiting...")
